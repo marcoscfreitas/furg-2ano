10606c2a2a4b65ab60dd7a341cbcd26c1d1e2be7
Marcos Vinicius <84063065+marcoscfreitas@users.noreply.github.com>
2025-04-23 18:33:15 +0000

    aula listas encadeadas

---
+++ algoritmos_e_estrutura_de_dados_2/estrutura_de_dados/lista_encadeamento/listaEncad.py
@@ -0,0 +1,83 @@
+class Nodo :
+    def __init__ (self, v) :
+        self.valor = v
+        self.prox = None
+
+class ListaEnc :
+    def __init__ (self) :
+        self.ini = None
+        self.fim = None
+        self.tam = 0
+
+    def inserir (self, posicao, valor) :
+        if posicao >= 0 :
+            if self.tam == 0 :
+                aux = Nodo(valor)
+                self.ini = aux
+                self.fim = aux
+                self.tam += 1
+            elif posicao == self.tam :
+                aux = Nodo(valor)
+                self.fim.prox = aux
+                self.fim = aux
+                self.tam += 1
+            elif posicao == 0 :
+                aux = Nodo(valor)
+                aux.prox = self.ini
+                self.ini = aux
+                self.tam += 1
+            else :
+                aux = Nodo(valor)
+                var = self.ini
+                for i in range(posicao) :
+                    anterior = var
+                    var = var.prox
+                aux.prox = var
+                anterior.prox = aux
+                self.tam += 1
+
+            return True
+        else :
+            return False
+
+    def imprimir (self) :
+        var = self.ini
+        while var.prox != None :
+            print(var.valor)
+            var = var.prox
+        if var.prox == None :
+            print(var.valor)
+
+    def verificaValor (self, posicao) :
+        if posicao <= self.tam :
+            cont = 0
+            var = self.ini
+            if var != None :
+                while var.prox != None :
+                    if cont == posicao :
+                        return var.valor
+                    var = var.prox
+                    cont += 1
+                if var.prox == None :
+                    return var.valor
+
+    # def verificaValor (self, valor) :
+    #     cont = 0
+    #     cont1 = self.tam
+    #     var = self.ini
+    #     while cont != cont1 :
+    #         if var.valor == valor :
+    #             return var.posicao
+        
+lista = ListaEnc()
+lista.inserir(0,'A')
+lista.inserir(1,'B')
+lista.inserir(2,'C')
+lista.inserir(0,'D')
+lista.inserir(2,'E')
+
+lista.imprimir()
+
+lista.verificaValor(1)
+
+# print(lista)
