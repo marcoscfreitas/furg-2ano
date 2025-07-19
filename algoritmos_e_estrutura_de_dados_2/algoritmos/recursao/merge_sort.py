257e7c296cdd9c18ccd11484a0ab7c6ff02967d3
Marcos Freitas <84063065+marcoscfreitas@users.noreply.github.com>
2025-07-19 15:58:44 +0000

    merge sort

---
+++ algoritmos_e_estrutura_de_dados_2/algoritmos/recursao/merge_sort.py
@@ -0,0 +1,39 @@
+import math
+
+def merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) // 2
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+
+    # Vetores auxiliares L e R
+    L = [0] * (n1 + 1)
+    R = [0] * (n2 + 1)
+
+    for i in range(n1):
+        L[i] = A[p + i]
+    for j in range(n2):
+        R[j] = A[q + 1 + j]
+
+    # Guardas infinitas
+    L[n1] = math.inf
+    R[n2] = math.inf
+
+    i = j = 0
+    for k in range(p, r + 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+# Exemplo de uso:
+A = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(A, 0, len(A) - 1)
+print("Vetor ordenado:", A)
